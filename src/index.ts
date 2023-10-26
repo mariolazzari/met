4aa3378a0eb246c129949b37d7f8f20c07a960fe
Mario Lazzari <mario.lazzari@gmail.com>
2023-10-26 21:23:38 +0200

    refact: exports

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
 export * from './Met';
+export * from './interfaces/DepartmentsResponse';
+export * from './interfaces/Result';
+export * from './interfaces/SearchRequest';
+export * from './interfaces/SearchRsponse';
 export * from './types/Department';
 export * from './types/ObjectData';
